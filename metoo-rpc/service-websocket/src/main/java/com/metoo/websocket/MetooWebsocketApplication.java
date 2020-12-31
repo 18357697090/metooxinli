f22ddd2a33e52cbb23d88e77d997493338eac18b
loongya <243570131@qq.com>
2020-12-31 18:08:53 +0800

    no message

--- metoo-rpc/service-websocket/src/main/java/com/metoo/websocket/MetooWebsocketApplication.java
+++ metoo-rpc/service-websocket/src/main/java/com/metoo/websocket/MetooWebsocketApplication.java
@@ -1,17 +1,15 @@
-package com.metoo.web;
+package com.metoo.websocket;
 
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @EnableDubbo
 @SpringBootApplication(scanBasePackages = {"com.metoo.websocket", "com.metoo.comm.config"})
 public class MetooWebsocketApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context  = SpringApplication.run(MetooWebsocketApplication.class, args);
-        SpringContextUtil.setApplicationContext(context);
+        SpringApplication.run(MetooWebsocketApplication.class, args);
     }
 }
 
