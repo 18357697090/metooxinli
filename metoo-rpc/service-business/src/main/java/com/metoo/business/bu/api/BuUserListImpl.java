96ed8bb5dc6217372cb8655d0359816bea5d3b71
jhxk <18357697090@163.com>
2021-01-06 17:35:57 +0800

    bu

--- metoo-rpc/service-business/src/main/java/com/metoo/business/bu/api/BuUserListImpl.java
+++ metoo-rpc/service-business/src/main/java/com/metoo/business/bu/api/BuUserListImpl.java
@@ -1,13 +1,20 @@
 package com.metoo.business.bu.api;
 
 import com.metoo.api.bu.BuUserListApi;
+import com.metoo.business.bu.dao.entity.BuUserList;
 import com.metoo.business.bu.service.BuUserListService;
+import com.metoo.pojo.bu.model.BuUserListModel;
+import com.metoo.pojo.im.model.ImUserMessageModel;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
+import org.dozer.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.annotation.Resource;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * <p>
@@ -18,13 +25,30 @@ import javax.annotation.Resource;
  * @since 2020-12-28
  */
 @Component
-@DubboService
 @Transactional
 @Slf4j
 public class BuUserListImpl implements BuUserListApi {
 
-    @Resource
+    @Autowired
     private BuUserListService buUserListService;
 
+    @Autowired
+    private Mapper mapper;
+
+
+    @Override
+    public List<BuUserListModel> findUserList(Integer page) {
+        page =page*10;
+        List<BuUserList> buUserLists = buUserListService.findUserList(page);
+        List<BuUserListModel> buUserListModels =buUserLists.stream().flatMap(e->{
+            return Stream.of(mapper.map(e, BuUserListModel.class));
+        }).collect(Collectors.toList());
+        return buUserListModels;
+    }
+
+    @Override
+    public long userCount() {
+        return buUserListService.userCount();
+    }
 
 }
--- metoo-rpc/service-business/src/main/java/com/metoo/business/bu/dao/repository/BuUserListRepository.java
+++ metoo-rpc/service-business/src/main/java/com/metoo/business/bu/dao/repository/BuUserListRepository.java
@@ -2,6 +2,13 @@ package com.metoo.business.bu.dao.repository;
 
 import com.metoo.business.bu.dao.entity.BuUserList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BuUserListRepository extends JpaRepository<BuUserList,Integer> {
+    long count();
+
+    @Query(value = "select * from bu_user_list limit ?1,10 ",nativeQuery = true)
+    List<BuUserList> findUserList(Integer page);
 }
